43f0bd02274cbe7c49f48c5ac6a4466ea6b0124f
Raving Genius <rg+code@ravinggenius.com>
2010-02-16 19:00:07 -0500

    Initial commit

---
+++ app.rb
@@ -0,0 +1,26 @@
+require 'rubygems'
+require 'compass'
+require 'sinatra'
+require 'haml'
+
+configure do
+  set :app_file, __FILE__
+  set :root, File.dirname(__FILE__)
+  set :haml, { :format => :html5, :attr_wrapper => '"' }
+
+  Compass.configuration do |config|
+    config.project_path = File.dirname(__FILE__)
+    config.sass_dir = 'views/stylesheets'
+  end
+
+  set :sass, Compass.sass_engine_options
+end
+
+get '/styles/:name.css' do
+  content_type :css, :charset => 'utf-8'
+  sass :"stylesheets/#{params[:name]}"
+end
+
+get '/' do
+  haml :index
+end
